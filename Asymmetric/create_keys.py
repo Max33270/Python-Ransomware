23b361eb12f7f56ce08d0053f7f83d094146f9f2
Maxime Doublait <@maxime.doublait>
2023-03-08 10:53:25 +0100

    files were not comitted in last commit

---
+++ Asymmetric/create_keys.py
@@ -0,0 +1,31 @@
+import cryptography
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+
+public_key = private_key.public_key()
+
+serial_private = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM, 
+    format=serialization.PrivateFormat.PKCS8, 
+    encryption_algorithm=serialization.NoEncryption()
+)
+
+with open("private_key.pem", "wb") as f:
+    f.write(serial_private)
+
+serial_pub = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+with open("public_key.pem", "wb") as f:
+    f.write(serial_pub)
+
+print("Private and public keys were created successfully!")
